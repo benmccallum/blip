8229abd94bdf3e20e54f8805985e1db161935bfe
Ben McCallum <ben.mccallum@live.com.au>
2017-09-03 12:27:12 -0600

    Added GA tracking to page views using vue-analytics plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ require('bootstrap');
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import router from './router';
 import App from './App';
 import './assets/styles/app.scss';
@@ -11,6 +12,12 @@ Vue.config.productionTip = false;
 
 // Use to 'go offline' and switch to offline mock data when needed
 window.offline = false;
+
+// Setup GA analytics with vue-analytics plugin (https://github.com/MatteoGabriele/vue-analytics)
+Vue.use(VueAnalytics, {
+  id: 'UA-105801869-1',
+  router
+});
 
 /* eslint-disable no-new */
 new Vue({
