7dfda2669434ce07e1d61694b7360a70f0cbfacb
Ben McCallum <ben.mccallum@live.com.au>
2017-09-03 18:30:25 -0600

    Added favicon.ico, more fun with webpack :P

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -55,6 +55,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
+      favicon: 'src/assets/images/favicon.ico',
       template: 'index.html',
       inject: true,
       minify: {
