e95e174f9a78d226ade3a32d3387d6dad5cbbfc1
Ben McCallum <ben.mccallum@live.com.au>
2020-03-15 13:27:32 +0100

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
   extends: [
-    'plugin:vue/vue3-recommended'
-  ]
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    // 'plugin:vue/vue3-recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    "vue/script-indent": ["error", 2, {
+      "baseIndent": 2,
+      "switchCase": 0,
+      "ignores": []
+    }]
+  }
 }
